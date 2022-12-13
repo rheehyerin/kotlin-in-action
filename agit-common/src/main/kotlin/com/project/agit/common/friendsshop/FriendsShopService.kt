fd8545258ab7803240bb3606408f8b9b3d84a6a1
senna-20220018 <senna-20220018@(1.5)-senna-20220018-MacBookPro>
2022-12-14 00:56:23 +0900

    [Senna] 프렌즈샵 추가 기능 구현

--- agit-common/src/main/kotlin/com/project/agit/common/friendsshop/FriendsShopService.kt
+++ agit-common/src/main/kotlin/com/project/agit/common/friendsshop/FriendsShopService.kt
@@ -1,14 +1,35 @@
 package com.project.agit.common.friendsshop
 
+import com.project.agit.common.friendsshop.dto.FriendsShopBook
+import com.project.agit.common.friendsshop.dto.FriendsShopBook.OrderBook
+import com.project.agit.common.friendsshop.dto.FriendsShopReqeust
+import com.project.agit.common.friendsshop.dto.FriendsShopResponse
+import com.project.agit.common.friendsshop.dto.ProductType
 import org.springframework.stereotype.Service
 
 @Service
 class FriendsShopService() {
-    fun getList(): String {
-        return "~ 제품이 있습니다"
+
+    fun buyProduct(product: FriendsShopReqeust) : String {
+        when (product.productType) {
+            ProductType.DOLL -> FriendsShopBook.Dolls
+                    .find { it.productName == product.productName }
+                    ?.let{ it.counts-- }  ?: throw IllegalArgumentException("No Dolls ${product.productName}")
+            ProductType.TUMBLER -> FriendsShopBook.TumberSets
+                .find { it.productName == product.personName }
+                ?.let{ it.counts-- } ?: throw IllegalArgumentException("No Tumbler the name of ${product.productName}")
+            else -> throw IllegalArgumentException("None Product Type ${product.productName}")
+        }
+        OrderBook.add("${product.personName}이 ${product.productName}을 구매하셨습니다")
+        return product.productName
     }
 
-    fun buyProduct(product: FriendsShopProduct) : String {
-        return "제품이 정상 구매 되었습니다"
+    fun showList() : List<String> {
+        var productList = mutableListOf<String>()
+        var iterator = FriendsShopBook.Dolls?.iterator()
+        while (iterator.hasNext())
+        {
+
+        }
     }
 }
