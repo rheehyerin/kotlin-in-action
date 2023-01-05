1c27288920e4acabb7e227055f474a1eac540bac
rheehyerin <toohappy2122@gmail.com>
2023-01-05 23:22:15 +0900

    디

--- flyway/migration/V1__initialize.sql
+++ flyway/migration/V1__initialize.sql
@@ -1,3 +1,18 @@
 create table test (
     `test` varchar(200) NOT NULL
 ) ENGINE=InnoDB;
+
+create table Product (
+    `ProductId` int NOT NULL,
+    `ProductType` char(1) NOT NULL,
+    `ProductName` varchar(100) NOT NULL,
+    `ProductPrice` int NOT NULL
+) ENGINE=InnoDB
+
+create table OrderHistory (
+    `Date` char(8) NOT NULL,
+    `OrderId` int NOT NULL,
+    `ProductId` int NOT NULL,
+    `EmployeeId` int NOT NULL,
+    `ProductPrice` int NOT NULL
+) ENGINE=InnoDB
